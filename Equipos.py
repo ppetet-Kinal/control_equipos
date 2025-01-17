30ac6fa25007e6892f078ae06872c34601c041a9
Pablo Petet <pablopetet2001@gmail.com>
2025-01-17 08:56:34 -0600

    Cambios

--- Equipos.py
+++ Equipos.py
@@ -1,24 +1,21 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
-from tkinter import filedialog
-import pandas as pd
 
 root = tk.Tk()
-root.title("Directorio Lexcom")
-root.geometry("500x400")
+root.title("Inventario de Sistemas")
+root.geometry("900x600")
 
-label_titulo = tk.Label(root, text="Directorio Lexcom", font=("Arial", 20))
+label_titulo = tk.Label(root, text="Control de Equipos", font=("Arial", 20))
 label_titulo.place(x=5, y=5)
 
 nb = ttk.Notebook(root)
 nb.place(x=10, y=50, width=880, height=540)
 
 pl = ttk.Frame(nb)
-nb.add(pl, text='Directorio')
+nb.add(pl, text='Control de Equipo')
 
-label_nombre = tk.Label(pl, text="Cartera", font=("Arial", 16))
-label_nombre.place(x=50, y=50)
-
+p2 = tk.Frame(nb)
+nb.add(p2, text='Importar')
 
 root.mainloop()
